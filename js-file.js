b74ec6bb5fee4a2aa4cac2e0ee6425eb22c19875
brycen <brycenbb1996@gmail.com>
2022-03-01 14:49:56 +0000

    start of project

---
+++ js-file.js
@@ -0,0 +1,13 @@
+const container = document.getElementsByClassName(".container");
+
+const div1 = document.createElement('div');
+
+// Need to make 4 rows, add to container, and in each row add 4 boxes
+
+for(let i = 0; i < 4; i++){
+    const row = document.createElement('div');
+    for(let j = 0; j < 4; j++){
+        const box = document.createElement('div');
+        row.appendChild(box);
+    }
+}
