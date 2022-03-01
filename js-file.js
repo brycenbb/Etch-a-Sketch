b86a07d0a154cbf189b68f7c5f56aea6ced3dbb6
brycen <brycenbb1996@gmail.com>
2022-03-01 15:51:52 +0000

    Added reset button functionality

--- js-file.js
+++ js-file.js
@@ -1,13 +1,39 @@
-const container = document.getElementsByClassName(".container");
+const container = document.querySelector('.container');
 
-const div1 = document.createElement('div');
-
-// Need to make 4 rows, add to container, and in each row add 4 boxes. Not sure yet
-
-for(let i = 0; i < 4; i++){
-    const row = document.createElement('div');
-    for(let j = 0; j < 4; j++){
-        const box = document.createElement('div');
+//Creates the grid of squares with a tiny border
+for(let i = 0; i < 16; i++){
+    let row = document.createElement('div');
+    row.style.display = "flex";
+    row.style.flexDirection = "row";
+    row.style.width = "640px";
+    row.style.height = "40px";
+    row.style.justifyContent = "center";
+    row.style.alignItems = "center";
+    container.appendChild(row);
+    for(let j = 0; j < 16; j++){
+        let box = document.createElement('div');
+        box.classList.add("boxes");
+        box.style.width = "40px";
+        box.style.height = "40px";
+        box.style.border = "1px solid black"
         row.appendChild(box);
     }
 }
+
+//Event handler for when mouse enters/exists
+
+const box = document.querySelectorAll('.boxes');
+for(let i = 0; i < box.length; i++){
+    box[i].addEventListener('mouseenter', function(){
+        box[i].classList.add("hover");
+    })
+}
+
+//Event handler for resetting the stage
+const button = document.querySelector('button');
+button.addEventListener('click',function() {
+    for(let i = 0; i < box.length; i++){
+        box[i].classList.remove("hover");
+    }
+})
+
