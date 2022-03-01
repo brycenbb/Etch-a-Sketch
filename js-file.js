f3b72a7b078b4b9f83e20850d68a263babeb0fee
brycen <brycenbb1996@gmail.com>
2022-03-01 14:54:20 +0000

    test

--- js-file.js
+++ js-file.js
@@ -2,7 +2,7 @@ const container = document.getElementsByClassName(".container");
 
 const div1 = document.createElement('div');
 
-// Need to make 4 rows, add to container, and in each row add 4 boxes
+// Need to make 4 rows, add to container, and in each row add 4 boxes. Not sure yet
 
 for(let i = 0; i < 4; i++){
     const row = document.createElement('div');
